931401856ca24b510bf728dd2a6fc2fa76118b59
psylon <stanislav.antunovich@yandex.ru>
2019-07-10 21:56:26 +0300

    1 В погодном приложении создайте еще один экран — экран для выбора города. На экране выбора города пользователь должен выбрать интересующий его город и по нажатию на кнопку переходить на уже сделанный вами экран отображения температуры. Как будут связаны и как будут взаимодействовать экраны выбора города и информации о температуре, мы узнаем из следующих занятий. Пока достаточно нарисовать экраны. 2 Экран выбора города может включать TextView для ввода города, список наиболее популярных городов и CheckBox для дополнительной информации: скорость ветра и давление. 3* Покажите этот экран вместо предыдущего.

--- app/src/main/java/ru/geekbrains/android1/MainActivity.java
+++ app/src/main/java/ru/geekbrains/android1/MainActivity.java
@@ -2,12 +2,23 @@ package ru.geekbrains.android1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public static String CITY = "CITY";
+    public static String WIND = "WIND";
+    public static String PRESSURE = "PRESSURE";
+    public static String HUMIDITY = "HUMIDITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_choice);
+    }
+
+    public void showWeather(View view) {
+        // заглушка
         setContentView(R.layout.activity_main);
     }
+
 }
