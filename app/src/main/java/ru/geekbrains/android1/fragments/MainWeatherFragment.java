2aec6646276ec1ec07f10bfd6c9d8a760f6d3cc9
psylon <stanislav.antunovich@yandex.ru>
2019-07-21 18:01:26 +0300

    adapter

--- app/src/main/java/ru/geekbrains/android1/fragments/MainWeatherFragment.java
+++ app/src/main/java/ru/geekbrains/android1/fragments/MainWeatherFragment.java
@@ -1,14 +1,17 @@
 package ru.geekbrains.android1.fragments;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearSnapHelper;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PagerSnapHelper;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import ru.geekbrains.android1.CityWeatherAdapter;
 import ru.geekbrains.android1.MainActivity;
@@ -16,8 +19,15 @@ import ru.geekbrains.android1.R;
 import ru.geekbrains.android1.data.WeatherDetailsData;
 
 public class MainWeatherFragment extends Fragment {
+    boolean isHorizontal;
     CityWeatherAdapter adapter; //TODO возможно вывести отедльный интерфейс
     RecyclerView weatherRecycler;
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        isHorizontal = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+    }
 
     @Nullable
     @Override
@@ -28,11 +38,13 @@ public class MainWeatherFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         String[] cities = view.getResources().getStringArray(R.array.cities);
+
         weatherRecycler = view.findViewById(R.id.main_weather_container);
+        weatherRecycler.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayout.HORIZONTAL, false));
         adapter = new CityWeatherAdapter(cities);
         weatherRecycler.setAdapter(adapter);
 
-        new LinearSnapHelper().attachToRecyclerView(weatherRecycler);
+        new PagerSnapHelper().attachToRecyclerView(weatherRecycler);
 
         super.onViewCreated(view, savedInstanceState);
     }
