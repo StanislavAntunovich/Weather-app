8bf166727c6e9edb9e13cb73a84216d2005efd8c
psylon <stanislav.antunovich@yandex.ru>
2019-07-27 14:25:44 +0300

    back button pressed from add city crash fixed

--- app/src/main/java/ru/geekbrains/android1/MainActivity.java
+++ app/src/main/java/ru/geekbrains/android1/MainActivity.java
@@ -3,12 +3,13 @@ package ru.geekbrains.android1;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import ru.geekbrains.android1.data.FakeSourceBuilder;
 import ru.geekbrains.android1.data.WeatherDataSource;
@@ -101,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == REQUEST_CODE || resultCode == RESULT_OK) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
             dataSource = (WeatherDataSource) data.getExtras().getSerializable(DATA_SOURCE);
         }
         super.onActivityResult(requestCode, resultCode, data);
